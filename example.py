940ed07388eda82d9dbd6872c390cdc16e27a633
zino <zino.gmail.com>
2021-06-09 17:36:34 +0000

    modified

--- example.py
+++ example.py
@@ -1,3 +1,3 @@
 def git_operation():
-   print("I am adding example.py file to the remote repository. ")
+   print("I am adding example.py file to th dsskde remote repository. hello ")
 git_operation()
